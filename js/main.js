c873ecbeddf27233ef94c927feee3ec644015259
JaloliddinKulmamatov <itjalol2010@gmail.com>
2024-03-16 22:27:30 +0500

    jalol

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-const API__URL = "https://api.escuelajs.co/api/v1/products"
-const wrapper = document.querySelector("main .wrapper")
+const API__URL = "https://dummyjson.com/products"
+const wrapper = document.querySelector(" .wrapper")
 const loading = document.querySelector(".loading")
 
 async function fetchData(api) {
     let data = await fetch(api)
     data 
     .json()
-    .then(res => createCard(res))
+    .then(res => createCard(res.products))
     .catch(err => console.log(err))
     .finally(() => {
         loading.style.display = "none"
@@ -15,17 +15,24 @@ async function fetchData(api) {
 fetchData(API__URL)
 
 function createCard(data) {
-    data.forEach(({title,images,price,id}) => {
-        let card = document.createElement("div")
-        card.classList.add("card")
-        card.innerHTML = `
-        <h3>${title}</h3>
-        <img src="${JSON.parse(images)[0]}" alt="">
-        <p>${price}</p>
-        `
-        card.addEventListener("click",() => singleRoute(id))
-        wrapper.appendChild(card)
-    });
+  data.forEach(({ title, images, price, id, brand,rating,discountPercentage }) => {
+    let card = document.createElement("div");
+    card.classList.add("wrapper__card");
+    card.innerHTML = `
+      <img src="${images[0]}" alt="${brand}">
+      <p>${rating}</p>
+      <h3>${title}</h3>
+      <span>
+      <p>${price}$ </p>
+      <p>${discountPercentage}% </p>
+      </span>
+    `;
+    
+
+    card.addEventListener("click", () => singleRoute(id));
+    
+    wrapper.appendChild(card);
+  });
 }
 
 function singleRoute(id) {
